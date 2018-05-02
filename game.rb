648d972b5dd47198824f5bbd19afba6c73ef0495
Nicholasblackburn1 <nickblackburn02@gmail.com>
2018-05-02 08:06:03 -0400

    first commit

---
+++ game.rb
@@ -0,0 +1,23 @@
+require 'rubygems'
+require 'gosu'
+
+
+class DemoWindow < Gosu::Window
+
+  def initialize
+    super(640, 400, false)
+    self.caption = "Demo"
+  end
+
+  def draw
+    x = 300
+    y = 200
+    size = 120
+    draw_quad(x-size, y-size, 0xffff8888, x+size, y-size, 0xffffffff, x-size, y+size, 0xffffffff, x+size, y+size, 0xffffffff, 0)
+  end
+
+end
+
+DemoWindow.new.show
+
+exit
